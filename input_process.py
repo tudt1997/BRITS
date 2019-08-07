ff55efdcdb8292e243db33118228f44535af0f9c
dbaranchuk <dmitrybaranchuk@gmail.com>
2019-08-07 20:08:55 +0200

    .

--- input_process.py
+++ input_process.py
@@ -133,7 +133,8 @@ def parse_id(id_):
     label = out.loc[int(id_)]
 
     rec = {'label': label}
-
+    print(label, type(label))
+    print(label.dtype)
     # prepare the model for both directions
     rec['forward'] = parse_rec(values, masks, evals, eval_masks, dir_='forward')
     rec['backward'] = parse_rec(values[::-1], masks[::-1], evals[::-1], eval_masks[::-1], dir_='backward')
