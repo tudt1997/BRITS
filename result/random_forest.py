91bce26ab3553f5dc96e8313189598dff03d013e
dbaranchuk <dmitrybaranchuk@gmail.com>
2019-08-08 11:22:36 +0200

    .

--- result/random_forest.py
+++ result/random_forest.py
@@ -13,6 +13,7 @@ n_train = 3000
 print(impute.shape)
 print(label.shape)
 
+from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import roc_auc_score
 from sklearn.linear_model import SGDClassifier
@@ -20,8 +21,11 @@ from sklearn.svm import LinearSVC
 
 auc = []
 
-for i in range(10):
+data = StandardScaler().fit_transform(data)
+
+for i in range(5):
     #model =  RandomForestClassifier().fit(data[:n_train], label[:n_train])
+
     model = LinearSVC(max_iter=10000, tol=1e-10).fit(data[:n_train], label[:n_train].ravel())
     pred = model.predict(data[n_train:])
     #auc.append(roc_auc_score(label[n_train:].reshape(-1,), pred[:, 1].reshape(-1, )))
