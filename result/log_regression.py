78a9ee0d2209f4336a5fc3c5679eee2d265b2d8b
dbaranchuk <dmitrybaranchuk@gmail.com>
2019-08-25 20:55:43 +0200

    check brits

--- result/log_regression.py
+++ result/log_regression.py
@@ -3,14 +3,17 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import roc_auc_score
 from sklearn.linear_model import LogisticRegression
 
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 model_name = 'brits'
 
 impute = np.load('./{}_data.npy'.format(model_name)).reshape(-1, 48 * 35)
-label = np.load('./{}_label.npy'.format(model_name))[3997:]
+label = np.load('./{}_label.npy'.format(model_name))#[3997:]
 
-data = np.nan_to_num(impute)[3997:]
+data = np.nan_to_num(impute)#[3997:]
 print(data.shape)
-n_train = data.shape[0] // 2
+n_train = 3000#data.shape[0] // 2
 
 print(impute.shape)
 print(label.shape)
