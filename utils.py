153d17c9fd7adf8595a53a2563f79d2e29cb65b8
tudt1997 <tudt1997@gmail.com>
2019-12-18 09:50:00 +0700

    Update custom models for multiclass classification

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 
 import pandas as pd
+import numpy as np
 
 def to_var(var):
     if torch.is_tensor(var):
@@ -35,3 +36,58 @@ def zero_var(sz):
     if torch.cuda.is_available():
         x = x.cuda()
     return x
+
+def parse_rec(values, masks, timestamps, evals, eval_masks, timestep, dir_):
+    deltas = parse_delta(masks, timestamps, timestep, dir_)
+
+    # only used in GRU-D
+    forwards = pd.DataFrame(values).fillna(method='ffill').fillna(0.0).to_numpy()
+    rec = {}
+
+    rec['values'] = np.nan_to_num(values).tolist()
+    rec['masks'] = masks.astype('int32').tolist()
+    # imputation ground-truth
+    rec['evals'] = np.nan_to_num(evals).tolist()
+    rec['eval_masks'] = eval_masks.astype('int32').tolist()
+    rec['forwards'] = forwards.tolist()
+    rec['deltas'] = deltas.tolist()
+
+    return rec
+
+def parse_delta(masks, timestamps, timestep, dir_):
+    if dir_ == 'backward':
+        masks = masks[::-1]
+
+    deltas = []
+
+    for h in range(timestep):
+        if h == 0:
+            deltas.append(np.zeros(masks.shape[1]))
+        else:
+            deltas.append((timestamps[h] - timestamps[h - 1]) * 100 + (1 - masks[h]) * deltas[-1])
+
+    return np.array(deltas)
+
+
+def collate_fn(recs):
+    forward = list(map(lambda x: x['forward'], recs))
+    backward = list(map(lambda x: x['backward'], recs))
+
+    def to_tensor_dict(recs):
+        values = torch.FloatTensor(list(map(lambda r: r['values'], recs)))
+
+        masks = torch.FloatTensor(list(map(lambda r: r['masks'], recs)))
+        deltas = torch.FloatTensor(list(map(lambda r: r['deltas'], recs)))
+
+        evals = torch.FloatTensor(list(map(lambda r: r['evals'], recs)))
+        eval_masks = torch.FloatTensor(list(map(lambda r: r['eval_masks'], recs)))
+        forwards = torch.FloatTensor(list(map(lambda r: r['forwards'], recs)))
+
+        return {'values': values, 'forwards': forwards, 'masks': masks, 'deltas': deltas, 'evals': evals, 'eval_masks': eval_masks}
+
+    ret_dict = {'forward': to_tensor_dict(forward), 'backward': to_tensor_dict(backward)}
+
+    ret_dict['labels'] = torch.FloatTensor(list(map(lambda x: x['label'], recs)))
+    # ret_dict['labels'] = torch.LongTensor(list(map(lambda x: x['label'], recs)))
+    ret_dict['is_train'] = torch.FloatTensor(list(map(lambda x: x['is_train'], recs)))
+    return ret_dict
